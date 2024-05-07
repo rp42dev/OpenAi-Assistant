8892a6fd6bbfcd13e9d442a0c3d7e240a3a18a0b
Raivis Petrovskis <rapet80@gmail.com>
2024-05-07 18:35:56 +0100

    added input

--- openai-assistant.py
+++ openai-assistant.py
@@ -1,68 +1,86 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
 
 # Set your OpenAI API key
 OPENAI_API_TOKEN = os.getenv('OPENAI_API_TOKEN') # 'your_api_key
-os.environ["OPENAI_API_KEY"] = OPENAI_API_TOKEN
 
 
 # Initialize the OpenAI client
 client = OpenAI(
-    # Set your OpenAI API key
     api_key = OPENAI_API_TOKEN
 )
 
 
-#return existing assistant if it exists
-def get_assistant(id):
-    assistant = client.beta.assistants.retrieve(id)
-    return assistant
-
-
-#create thread for assistant
-def create_thread(vs_ID):
+#load openAi assistant and vector store
+def load_openai_assistant(assistant_id, vs_ID):
+    assistant = client.beta.assistants.retrieve(assistant_id)
     thread = client.beta.threads.create(
-        messages=[ { "role": "user", "content": "what price"} ],
-            tool_resources={
-                "file_search": {
-                "vector_store_ids": [vs_ID]
-                }
+        tool_resources={
+            "file_search": {
+            "vector_store_ids": [vs_ID]
             }
+        }
+    )
+    return thread, assistant
+
+
+# check in loop  if assistant ai parse our request
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
         )
-    return thread
+        time.sleep(0.5)
+    return run
+
+
+# initiate assistant ai response
+def get_assistant_response(thread, assistant_id, user_input):
+
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=user_input,
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+    )
+
+    run = wait_on_run(run, thread)
+
+    # Retrieve all the messages added after our last user message
+    messages = client.beta.threads.messages.list(
+        thread_id=thread.id, order="asc", after=message.id
+    )
+
+    return messages.data[0].content[0].text.value
 
 
 #main function
 def main():
     vs_ID = "vs_dOpDSOSZrEYh5YhGcNJS5YLX"
     assistant_id = "asst_FzT6VbdmcKBXMuG9NqHkKyLn"
-    assistant = get_assistant(assistant_id)
-    thread = create_thread(vs_ID)
-
-    # the run until it's in a terminal state.
-    run = client.beta.threads.runs.create_and_poll(
-        thread_id=thread.id, assistant_id=assistant.id
-    )
-
-    messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
-
-    message_content = messages[0].content[0].text
-    annotations = message_content.annotations
-    citations = []
-    for index, annotation in enumerate(annotations):
-        message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
-        if file_citation := getattr(annotation, "file_citation", None):
-            cited_file = client.files.retrieve(file_citation.file_id)
-            citations.append(f"[{index}] {cited_file.filename}")
-
-    print(message_content.value)
-    print("\n".join(citations))
-
-
+    thread, assistant = load_openai_assistant(assistant_id, vs_ID)
+    
+    
+    while True:
+        user_input = input("You: ")
+        if user_input == "exit":
+            break
+        response = get_assistant_response(thread, assistant_id, user_input)
+        print("Assistant:", response)
+        
+    print("Exiting...")
+    return
+    
 if __name__ == "__main__":
     main()
 
